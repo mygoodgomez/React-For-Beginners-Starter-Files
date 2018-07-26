a9ba0eeed356a42c1486787c724ec56985b05fe9
Nik Gomez <nik.gomez@moat.com>
2018-07-25 21:17:14 -0400

    finished lesson on React Router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
 import { render } from 'react-dom';
-import App from './components/App.js';
+import Router from './components/Router.js';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
