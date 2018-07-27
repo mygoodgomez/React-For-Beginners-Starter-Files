df0d0ea041c97d4662f15d293af0f3d6b39fa117
Nik Gomez <nik.gomez@moat.com>
2018-07-27 15:59:51 -0400

    finished lesson on order and unavailable items

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -15,12 +15,15 @@ class App extends React.Component {
         console.log(fish);
         const fishes = {...this.state.fishes};
         fishes[`fish${Date.now()}`] = fish;
-        this.setState({
-            fishes: fishes
-        });
+        this.setState({fishes});
     };
     loadSampleFishes = () => {
         this.setState({fishes: sampleFishes});
+    };
+    addToOrder = (key) => {
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({order});
     };
     render() {
         return (
@@ -28,10 +31,10 @@ class App extends React.Component {
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                     <ul className="fishes">
-                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)}
                     </ul>
                 </div>
-                <Order />
+                <Order order={this.state.order} fishes={this.state.fishes} />
                 <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
             </div>
         )
